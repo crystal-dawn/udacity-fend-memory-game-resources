88ba864de2dc75888e66af2e7ddefe903e13520c
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2018-12-22 20:58:03 -0500

    Feat: array for resources

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/**
+* @param {array} resources - collection of resources
+*/
+const resources = [{
+  requirement: "Game Behavior",
+  type: "Blog/Article",
+  page: "How to handle event handling in JavaScript",
+  url: "https://medium.freecodecamp.org/event-handling-in-javascript-with-examples-f6bc1e2fff57",
+  sharer: "Linda"
+}]
