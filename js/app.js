937272dd79b7fe288d60318e056b097eaa7ac750
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2018-12-23 01:47:42 -0500

    feat: resource button

--- js/app.js
+++ js/app.js
@@ -180,70 +180,24 @@ const mainElement = () => {
     // add class name to element
     .classList.add('requirements');
 
-  sectionElement();
-}
-
-/**
- * @description loop through array of requirements to create each section
- * @todo if there are no resources for a requirement, don't create section
- */
-const sectionElement = () => {
-  for (i = 0; i < requirementsList.length; i++) {
-    for (r = 0; r < resources.length; r++) {
-      if (resources[r].requirement === requirementsList[i]) {
-        const requirement = document.querySelector('main')
-          .insertAdjacentHTML('afterbegin', `<section class="requirement"></section>`);
-
-        requirementHeader();
-      }
-    }
-  }
-}
-
-/**
- * @description add requirement text in h2 tag
- */
-const requirementHeader = () => {
-  document.querySelector('.requirement')
-    .insertAdjacentHTML('beforeend', `<h2 class="requirement-header">${requirementsList[i]}</h2>`);
-
-  resourceRequirements();
-}
-
-/**
- * @description check if requirement has a type section
- * @param {array} resources
- * @param {array} requirements
- */
-const resourceRequirements = () => {
-  console.log(resources[r].requirement, requirementsList[i], resources[r].requirement === requirementsList[i]);
-  resourceType();
-}
-
-/**
- * @description add type section
- */
-const resourceType = () => {
-  document.querySelector('h2')
-  .insertAdjacentHTML('afterend', `<section class="type">
-    <h3 class="type-header">${resources[r].type}</h3></section>`);
-
   resourceCard();
 }
 
 /**
- * @description add resource cards
- * @todo sharer empty fallback
+ * @description loop through array of resources creating buttons for each
  */
 const resourceCard = () => {
-  document.querySelector('h3')
-  .insertAdjacentHTML('afterend',
-    `<button class="resource">
-    <a class="link" href=${resources[r].url}>${resources[r].page}
-    </br>
-    <span class="credit">Shared by: @${resources[r].sharer}</span>
-    </a>`
-  )
+  for (r = 0; r < resources.length; r++) {
+    document.querySelector('main')
+      .insertAdjacentHTML('afterbegin',
+      `<button class="resource">
+        <a class="link" href=${resources[r].url}>
+          ${resources[r].page}</br>
+          <span class="credit">Shared by: @${resources[r].sharer}</span>
+        </a>
+      </button>`);
+    // requirementHeader();
+  }
 }
 
 document.body.onload = loadPage();
