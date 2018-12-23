8b8eb859c4e0bcab797dabeb5c068f5432b1bc97
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2018-12-22 21:37:14 -0500

    Feat: Main element for resource requirement cards

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /**
-* @param {array} resources - collection of resources
-*/
+ * @param {array} resources - collection of resources
+ */
 const resources = [{
   requirement: "Game Behavior",
   type: "Blog/Article",
@@ -8,3 +8,23 @@ const resources = [{
   url: "https://medium.freecodecamp.org/event-handling-in-javascript-with-examples-f6bc1e2fff57",
   sharer: "Linda"
 }]
+
+/**
+* @description add main element for start of requirement cards
+*/
+const main = document.createElement('main');
+const requirements = document.body.appendChild(main)
+  // add class name to element
+  .classList.add('requirements');
+
+/**
+* @description nest section element in main
+*/
+const requirement = document.querySelector('main')
+  .insertAdjacentHTML('beforeend', `<section class="requirement"></section>`);
+
+/**
+* @description add requirement text in h2 tag
+*/
+const requirementHeader = document.querySelector('.requirement')
+  .insertAdjacentHTML('beforeend', `<h2 class="requirement-header">${resources[0].requirement}</h2>`);
