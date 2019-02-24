49f611fa9a301a88a224006fe83935f6297ab263
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-02-24 01:43:32 -0500

    feat: Update styling

--- js/app.js
+++ js/app.js
@@ -1,167 +1,4 @@
 /**
- * @param {array} resources - collection of resources
- * @todo group resources by requirement and type instead of having separate
- *       objects
- */
-const resources = [{
-    requirement: "Game Behavior",
-    type: "Blog/Article",
-    page: "How to handle event handling in JavaScript",
-    url: "https://medium.freecodecamp.org/event-handling-in-javascript-with-examples-f6bc1e2fff57",
-    sharer: "Linda"
-  },
-  {
-    requirement: "Timer",
-    type: "Tutorial",
-    page: "JavaScript Timing Events",
-    url: "https://www.w3schools.com/js/js_timing.asp",
-    sharer: "Sara Krum"
-  },
-  {
-    requirement: "Game Behavior",
-    type: "Documentation",
-    page: "Using CSS transitions",
-    url: "hhttps://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions",
-    sharer: "Amy"
-  },
-  {
-    requirement: "Game Behavior",
-    type: "Documentation",
-    page: "::before (:before)",
-    url: "https://developer.mozilla.org/en-US/docs/Web/CSS/::before",
-    sharer: "Anthony (FEND)"
-  },
-  {
-    requirement: "Congratulations Popup & Star Rating",
-    type: "Answer",
-    page: "What is the difference between visibility:hidden and display:none?",
-    url: "https://stackoverflow.com/questions/133051/what-is-the-difference-between-visibilityhidden-and-displaynone",
-    sharer: "Ivan.ProjectCoach [FEND]"
-  },
-  {
-    requirement: "Game Behavior",
-    type: "Documentation",
-    page: "Psuedo-elements",
-    url: "https://developer.mozilla.org/en-US/docs/Web/CSS/Pseudo-elements",
-    sharer: "Anthony (FEND)"
-  },
-  {
-    requirement: "Timer",
-    type: "Tutorial",
-    page: "Practice Timer - A PEN BY Chris Neal",
-    url: "https://codepen.io/chrisvneal/pen/OEMJyR?editors=0010",
-    sharer: "Chris N [FEND]"
-  },
-  {
-    requirement: "Congratulations Popup",
-    type: "Tutorial",
-    page: "How TO - CSS/JS Modal",
-    url: "https://www.w3schools.com/howto/howto_css_modals.asp",
-    sharer: "Ivan.ProjectCoach [FEND]"
-  },
-  {
-    requirement: "Congratulations Popup & Star Rating",
-    type: "Documentation",
-    page: "display",
-    url: "https://developer.mozilla.org/en-US/docs/Web/CSS/display",
-    sharer: ""
-  },
-  {
-    requirement: "Star Rating",
-    type: "Tutorial",
-    page: "HTML DOM appendChild() Method",
-    url: "https://www.w3schools.com/jsref/met_node_appendchild.asp",
-    sharer: ""
-  },
-  {
-    requirement: "Timer",
-    type: "Documentation",
-    page: "WindowOrWorkerGlobalScope.setInterval()",
-    url: "https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval",
-    sharer: ""
-  },
-  {
-    requirement: "Timer",
-    type: "Library",
-    page: "husa/timer.js",
-    url: "https://github.com/husa/timer.js/",
-    sharer: "Crystal"
-  },
-  {
-    requirement: "Game Behavior",
-    type: "Blog/Article",
-    page: "How to manipulate classes without jQuery by using HTML5's classList API",
-    url: "https://medium.freecodecamp.org/how-to-manipulate-classes-using-the-classlist-api-f876e2f58236",
-    sharer: ""
-  },
-  {
-    requirement: "Interface Design",
-    type: "Library",
-    page: "FONT AWESOMEv.4.7.0",
-    url: "https://fontawesome.bootstrapcheatsheets.com/",
-    sharer: "Sara Krum"
-  },
-  {
-    requirement: "Game Behavior",
-    type: "Blog/Article",
-    page: "Things you should know about JS events",
-    url: "https://medium.com/@pierreda/things-you-should-know-about-js-events-4ab474312736",
-    sharer: "Linda"
-  },
-  {
-    requirement: "Game Behavior",
-    type: "Answer",
-    page: "Confused on querySelector and getElementById",
-    url: "https://www.reddit.com/r/learnjavascript/comments/356k1v/confused_on_queryselector_and_getelementbyid/",
-    sharer: "Marsh[FEND]"
-  },
-  {
-    requirement: "Game Behavior",
-    type: "Documentation",
-    page: "EventListener",
-    url: "https://developer.mozilla.org/en-US/docs/Web/API/EventListener",
-    sharer: "Mitali {FEND}"
-  },
-  {
-    requirement: "Game Behavior",
-    type: "Tutorial",
-    page: "JavaScript HTML DOM EventListener",
-    url: "https://www.w3schools.com/js/js_htmldom_eventlistener.asp",
-    sharer: "Mitali {FEND}"
-  },
-  {
-    requirement: "Game Behavior",
-    type: "Book",
-    page: "Handling Events",
-    url: "https://eloquentjavascript.net/15_event.html",
-    sharer: "Mitali {FEND}"
-  }
-]
-
-/**
- * @todo remove unused requirements dynamically
- */
-const requirementsList = [
-  "Game Behavior",
-  // "Memory Game Logic",
-  "Congratulations Popup & Star Rating",
-  "Congratulations Popup",
-  // "Restart Button",
-  // "Star Rating",
-  "Timer",
-  // "Move Counter",
-  // "Interface Design",
-  // "Documentation",
-];
-
-const types = [
-  "Tutorial",
-  "Documentation",
-  "Blog/Article",
-  "Answer"
-];
-
-/**
  * @description create main element when page loads
  * @todo Try and do IFFE for page loads
  */
@@ -170,62 +7,65 @@ const loadPage = () => {
 }
 
 /**
- * @description add main element for start of requirement cards
+ * @description add main element for start of criteria cards
  */
 const mainElement = () => {
   document.querySelector('nav')
-  .insertAdjacentHTML('afterend', `<main class="requirements"></main>`);
+    .insertAdjacentHTML('afterend', `<main class="criterias"></main>`);
 
-  requirementCard();
+  criteriaCard();
 }
 
 /**
- * @description loop through array of requirements creating each section
+ * @description loop through array of criterias creating each section
  * @todo remove empty cards
  */
-const requirementCard = () => {
-  for (i = 0; i < requirementsList.length; i++) {
-    const requirements = requirementsList.map((requirement, index) => {
+const criteriaCard = () => {
+  for (i = 0; i < criteriasList.length; i++) {
+    const criterias = criteriasList.map((criteria, index) => {
       document.querySelector('main')
         .insertAdjacentHTML('afterbegin',
-          `<section class="requirement">
-          <h2 class="requirement-header" id="requirement${index}">${requirement}</h2>
-        </section>`);
+          `<section class="criteria">
+             <div id="ctn${index}" style="position:relative;">
+               <a id="criteria${index}">&nbsp;</a>
+               <h2 class="criteria-header">${criteria}</h2>
+             </div>
+           </section>`);
 
-            document.querySelectorAll('span')[1].insertAdjacentHTML('afterbegin',
-              `<a class="dropdown-content-link" href="#requirement${index}">${requirement}</a>`)
-      typeCard(requirement);
+      document.querySelector('ul').insertAdjacentHTML('afterbegin',
+        `<li class="criteria-list">
+           <a class="dropdown-link" href="#criteria${index}">${criteria}</a>
+        </li>`)
+      typeCard(criteria);
     })
-
   }
 }
 
 /**
  * @description create type cards for each type of resources
  */
-const typeCard = (requirement) => {
+const typeCard = (criteria) => {
   types.map(type => {
-    document.querySelector('h2')
+    document.querySelector('section > div ')
       .insertAdjacentHTML('afterend',
         `<section class="type">
           <h3 class="type-header">${type}</h3>
-          <br/>
         `);
-    resourceCard(requirement, type);
+    resourceCard(criteria, type);
   })
 }
 
 /**
  * @description create resource card buttons for each resource
  */
-const resourceCard = (requirement, type) => {
+const resourceCard = (criteria, type) => {
   for (i = 0; i < resources.length; i++) {
-    if (type === resources[i].type && requirement === resources[i].requirement) {
+    if (type === resources[i].type && criteria === resources[i].criteria) {
       document.querySelector('h3')
         .insertAdjacentHTML('afterend', `
-          <button class="resource">
-            <a class="link" href=${resources[i].url}>
-              <h4 class="link-header">${resources[i].page}</h4>
+          <button class="resource" tabindex="-1">
+            <a class="resource-link" href=${resources[i].url}>
+              ${resources[i].page}</br>
               <span class="credit">Shared by: ${resources[i].sharer}</span>
             </a>
            </button>
